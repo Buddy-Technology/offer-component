a9de228f80fa5d02f57badc2128e295fa16d75c3
Cezar Carvalhaes <cezarcarvalhaes@gmail.com>
2022-05-09 11:52:44 -0400

    feat: offer component set up

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function App(options) {
+
+  const onBuddyScriptLoad = (e) => {
+    window.buddy_setup(options);
+  };
+
+  const loadScript = (env) => {
+    if (!window.buddy_setup) {
+      const scriptUrl = env === 'PRODUCTION' 
+        ? 'https://embed.buddyinsurance.com/scripts/v2/index.js'
+        : 'https://staging.embed.buddyinsurance.com/scripts/v2/index.js'
+      const s = document.createElement('script');
+      s.src = scriptUrl;
+      s.id = 'buddy-script'
+      s.onload = onBuddyScriptLoad;
+      document.body.appendChild(s);
+    }
+  };
+  loadScript(options.stage || 'STAGING');
+
+  return (
+    <div id="buddy_widget">
+    </div>
+  );
+}
+
+export default App;
