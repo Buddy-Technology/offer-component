18c547a9caf7ac27449a30223f3a8de6fca8b4c8
Cezar Carvalhaes <cezarcarvalhaes@gmail.com>
2022-05-10 15:01:12 -0400

    chore: add jsdoc to util funcs

--- src/util.js
+++ src/util.js
@@ -4,28 +4,54 @@ const V3_URL = 'http://localhost:3012/index.js';
 const V3_URL_REGEX = /^http:\/\/localhost:3012\/index\.js\/?(\?.*)?$/;
 const EXISTING_SCRIPT_MESSAGE = 'Buddy Script is already present';
 
+/**
+ * @type {Object}
+ * @private
+ * @property {string} stage
+ */
 const defaultOptions = {
 	stage: 'STAGING',
 };
 
+/**
+ * @function findScript
+ * @private
+ * @returns {(HTMLElement | undefined)}
+ */
 export const findScript = () => {
 	const scripts = Array.from(document.querySelectorAll(`script[src^="${V3_URL}"]`));
 	const matchedScript = scripts.find(({ src }) => V3_URL_REGEX.test(src));
 	return matchedScript;
 };
 
+/**
+ * @function updateOffer
+ * @private
+ * @param  {Object} options
+ */
 export const updateOffer = (options) => {
 	if (window.Buddy) {
 		window.Buddy.updateOffer(options);
 	}
 };
 
+/**
+ * @function createOffer
+ * @private
+ * @param  {Object} options
+ */
 const createOffer = (options) => {
 	if (window.Buddy) {
 		window.Buddy.createOffer(options);
 	}
 };
 
+/**
+ * @function injectScript
+ * @private
+ * @param  {Object} options
+ * @returns {HTMLElement}
+ */
 const injectScript = (options) => {
 	const finalOptions = { defaultOptions, ...options };
 	const script = document.createElement('script');
@@ -35,8 +61,18 @@ const injectScript = (options) => {
 	return script;
 };
 
+/**
+ * @type {(Promise | null)}
+ * @private
+ */
 let scriptPromise = null;
 
+/**
+ * @function loadScript
+ * @private
+ * @param  {Object} options
+ * @returns {Promise}
+ */
 export const loadScript = (options) => {
 	// Ensure that we only attempt to script once
 	if (scriptPromise !== null) {
